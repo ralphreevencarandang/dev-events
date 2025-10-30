c9cf90eef8392b1999753bc08b1e6269378af684
Archi RR <architeam.carandangr@gmail.com>
2025-10-30 14:08:03 +0800

    Initial Commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <section>
+        <h1 className='text-center'>The Hub for Every Dev <br /> Event You Can't Miss</h1>
+        <p className='text-center mt-5'>Hackatons, Meetups, and Conferences, All in One Place</p>
+    </section>
+  )
+}
+
+export default page
